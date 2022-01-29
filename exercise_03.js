9ac47d7e1a7b97dee2c9f6308d4a899f5c892e21
anabeatrizalmeida <ana-almeida@hotmail.com>
2022-01-29 19:16:22 -0300

    exercise_03

---
+++ exercise_03.js
@@ -0,0 +1,15 @@
+var prompt  = require(`prompt-sync`)();
+
+const musica = (
+    `
+    Baby, I'm dancing in the dark
+    With you between my arms
+    Barefoot on the grass
+    Listening to our favorite song
+    When you said you looked a mess
+    I whispered underneath my breath
+    But you heard it
+    Darling, you look perfect tonight`
+);
+
+console.log(musica);
